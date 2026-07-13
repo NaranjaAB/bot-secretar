f1829446f32ee3a8c36d499e83bcd91197be9dcd
NaranjaAB <abattakova@gmail.com>
2026-07-13 23:38:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher 
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.filters import CommandStart, Command, StateFilter
 import asyncio
